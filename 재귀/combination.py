a34637017f81d9ebbe61ea0d9e1d881fa25584ef
yurjune <yurjune@gihub.com>
2024-08-18 15:58:42 +0900

    순열, 조합

---
+++ 재귀/combination.py
@@ -0,0 +1,22 @@
+def comb(arr, k):
+    res = []
+    if k == 0:
+        return [[]]
+
+    for i, el in enumerate(arr):
+        rest = arr[i+1:]
+
+        for c in comb(rest, k-1):
+            res.append([el] + c)
+    
+    return res
+    
+print(comb([1,2,3,4], 3))
+
+def comb_count(n, k):
+    if k == 0:
+        return 1
+    if n < k:
+        return 0
+
+    return comb_count(n-1, k) + comb_count(n-1, k-1)
---
+++ 재귀/permutation.py
@@ -0,0 +1,22 @@
+def perm(arr, k):
+    res = []
+    if k == 0:
+        return [[]]
+
+    for i, el in enumerate(arr):
+        rest = arr[:i] + arr[i+1:]
+
+        for p in perm(rest, k-1):
+            res.append([el] + p)
+    
+    return res
+
+print(perm([1,2,3,4], 3))
+
+def perm_count(n, k):
+    if k == 0:
+        return 1
+    if n < k:
+        return 0
+    
+    return n * perm_count(n-1, k-1)
